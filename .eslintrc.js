133f95e427e871ec5131c532701e9783e5843f52
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2022-08-16 20:49:40 -0300

    seeds

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,6 +15,7 @@ module.exports = {
     'class-methods-use-this': 'off',
     'import/first': 'off',
     'no-param-reassign': 'off',
+    'no-empty-function': 'off',
     'no-trailing-spaces': 'off',
   },
 };
---
+++ src/database/seeds/20220816234055-create-users.js
@@ -0,0 +1,31 @@
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.bulkInsert('users', [
+      {
+        name: 'Luiz',
+        email: 'luiz@gmail.com',
+        password_hash: await bcrypt.hash('afsdfqgrwg2w43t', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Dorival',
+        email: 'dorival@gmail.com',
+        password_hash: await bcrypt.hash('asdfaggqwg2vba', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        name: 'Tai',
+        email: 'tai@gmail.com',
+        password_hash: await bcrypt.hash('q3ty4gadfb3bab', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+    ], {});
+  },
+
+  async down() {},
+};
