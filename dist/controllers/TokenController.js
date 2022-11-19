d6ca57e40e26e6c23e247c1b72f53f14003a6155
Ronan Vieira do Carmo Junior <ronanvcjunior@gmail.com>
2022-11-19 08:53:44 -0300

    Adicionando cors

--- dist/controllers/TokenController.js
+++ dist/controllers/TokenController.js
@@ -31,7 +31,7 @@ class TokenController {
         expiresIn: process.env.TOKEN_EXPIRANTION,
       });
 
-      return res.json({ token });
+      return res.json({ token, user: { nome: user.nome, id, email } });
     } catch (error) {
       return res.status(400).json({
         errors: error.errors.map((err) => err.message),
